ab5a5a5990a7a7a3be980635630194af70fb3b59
benten <ben@benten.ca>
2022-06-01 14:49:32 -0600

    hooked up projects to sanity

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -2,55 +2,79 @@ import { FiExternalLink } from 'react-icons/fi'
 import { SiGithub } from 'react-icons/si'
 import { MdArticle } from 'react-icons/md'
 
-export default function Projects() {
+export async function getStaticProps() {
+    const res = await fetch("https://6po0ek1e.api.sanity.io/v2021-10-21/data/query/production?query=%7B'featured'%3A*%5B_type%20%3D%3D%20%22project%22%20%26%26%20featured%20%3D%3D%20true%5D%7B'image'%3AmainImage.asset-%3Eurl%2C%20title%2C%20body%2C%20github%2C%20external%2C%20blog%20%7D%2C%0A%22projects%22%3A*%5B_type%20%3D%3D%20%22project%22%20%26%26%20featured%20%3D%3D%20false%5D%7Btitle%2C%20body%2C%20github%2C%20external%2C%20blog%20%7D%7D")
+    const posts = await res.json()
+    return {
+        props: {
+            posts,
+        },
+    }
+}
 
+export default function Projects(props: any) {
     return (
         <section className="section space-y-10">
             <h1 className='page-title'>Projects</h1>
-            <FeaturedProject />
-            <FeaturedProject />
+            {props.posts.result.featured.map((p: any) => <FeaturedProject project={p} />)}
             <div className="grid md:grid-cols-2 gap-4">
-                <Project />
-                <Project />
-                <Project />
-                <Project />
+                {props.posts.result.projects.map((p: any) => <Project project={p} />)}
             </div>
         </section>
     )
 }
 
-function FeaturedProject() {
+function FeaturedProject(project: any) {
+    console.log(project)
     return (
-        <a className='block relative w-full p-4 md:p-6 bg-background-focus rounded-lg'>
+        <div className='block relative w-full p-4 md:p-6 bg-background-focus rounded-lg'>
             <img className='h-[496px] w-full object-cover rounded-md' src='/assets/ChickChau.jpg' />
             <div className='flex flex-col mt-4 md:flex-row justify-between'>
-                <h2 className='text-3xl'>Project Name here</h2>
+                <h2 className='text-3xl'>{project.project.title}</h2>
                 <div className='flex space-x-3 mt-4 md:mt-0'>
-                    <FiExternalLink className='w-10 h-10 stroke-primary' />
-                    <SiGithub className='w-10 h-10 fill-secondary' />
-                    <MdArticle className='w-10 h-10 fill-accent' />
+                    {project.project.external &&
+                        <a rel='external' target='_blank' href={project.project.external}>
+                            <FiExternalLink className='w-10 h-10 transition hover:stroke-primary' />
+                        </a>}
+                    {project.project.github &&
+                        <a rel='external' target='_blank' href={project.project.github}>
+                            <SiGithub className='w-10 h-10 stroke-primary transition hover:fill-secondary' />
+                        </a>}
+                    {project.project.blog &&
+                        <a rel='external' target='_blank' href={project.project.blog}>
+                            <MdArticle className='w-10 h-10 stroke-primary transition hover:fill-accent' />
+                        </a>}
                 </div>
             </div>
             <p className='text-base mt-10'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum voluptate minima molestias illum ratione distinctio optio architecto exercitationem, ex odio quaerat vel libero numquam, modi nostrum saepe tempora ab! Enim magnam est officiis molestias nesciunt repudiandae rerum molestiae animi illum nobis repellat repellendus, a doloribus vitae ea. Culpa doloribus iusto voluptatum voluptatem commodi quos sapiente eius reiciendis, voluptates praesentium repudiandae illum quam rem error distinctio, ea modi ad ducimus assumenda vitae omnis ipsa. Corporis minima ex vel in? Hic non sunt iure totam suscipit, dolore quaerat consequuntur animi natus molestiae maiores recusandae consectetur at dignissimos mollitia aut illum et accusantium.
+                {project.project.body}
             </p>
-        </a>
+        </div>
     )
 }
-function Project() {
+function Project(project: any) {
     return (
-        <a className='block relative w-full p-4 bg-background-focus rounded-lg'>
+        <div className='block relative w-full p-4 bg-background-focus rounded-lg'>
             <div className='flex justify-between mt-4'>
-                <h2 className='text-2xl'>Project Name here</h2>
+                <h2 className='text-2xl'>{project.project.title}</h2>
                 <div className='flex space-x-3'>
-                    <FiExternalLink className='w-8 h-8 stroke-primary' />
-                    <SiGithub className='w-8 h-8 fill-secondary' />
-                    <MdArticle className='w-8 h-8 fill-accent' />
+                    {project.project.external &&
+                        <a rel='external' target='_blank' href={project.project.external}>
+                            <FiExternalLink className='w-8 h-8 transition hover:stroke-primary' />
+                        </a>}
+                    {project.project.github &&
+                        <a rel='external' target='_blank' href={project.project.github}>
+                            <SiGithub className='w-8 h-8 stroke-primary transition hover:fill-secondary' />
+                        </a>}
+                    {project.project.blog &&
+                        <a rel='external' target='_blank' href={project.project.blog}>
+                            <MdArticle className='w-8 h-8 stroke-primary transition hover:fill-accent' />
+                        </a>}
                 </div>
             </div>
             <p className='text-sm mt-5'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum voluptate minima molestias illum ratione distinctio optio architecto exercitationem, ex odio quaerat vel libero numquam, modi nostrum saepe tempora ab!
+                {project.project.body}
             </p>
-        </a>
+        </div>
     )
 }
